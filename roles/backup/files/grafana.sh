ee221a2a65337c27116058e7159d6b001849bacd
CybRookie <mikara@ttu.ee>
2020-12-16 12:26:46 +0200

    Final Lab - Create playbook to setup the whole infrastructure - Create playbook to delete backup users as a fix - Update all the roles to accommodate 3rd VM - Update the index HTML page on the Nginx for the management VM

--- roles/backup/files/grafana.sh
+++ roles/backup/files/grafana.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -a /etc/grafana/ /home/backup/backup/grafana.etc/ &> /dev/null
-rsync -a /var/lib/grafana/ /home/backup/backup/grafana.lib/ &> /dev/null
+#rsync -a /etc/grafana/ /home/backup/backup/grafana.etc/ &> /dev/null
+#rsync -a /var/lib/grafana/ /home/backup/backup/grafana.lib/ &> /dev/null
 
 # Added backup procedures for the containerised Grafana directories, since Lab 12 - Docker
 
